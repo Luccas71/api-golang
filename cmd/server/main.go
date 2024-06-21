9a6f6275b3e9a9122c026b09bca0a2b6f1bfaede
Lucas Volpato <lmaciel916@gmail.com>
2024-06-21 14:43:57 +0000

    feat: add .env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,12 @@
 package main
 
-func main(){}
+import (
+	"fmt"
+
+	"github.com/Luccas1/api-golang/configs"
+)
+
+func main() {
+	config, _ := configs.LoadConfig(".")
+	fmt.Println(config.DBHost)
+}
