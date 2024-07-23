0c62d0614cddb3f9c4bb81619b8fbc52cb1c7f7c
Lucas Volpato <lmaciel916@gmail.com>
2024-07-23 10:31:15 +0000

    reset api

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,46 +1,7 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/Luccas1/api-golang/configs"
-	"github.com/Luccas1/api-golang/internal/entity"
-	"github.com/Luccas1/api-golang/internal/infra/database"
-	"github.com/Luccas1/api-golang/internal/infra/webserver/handlers"
-	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/chi/v5"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-)
+import ()
 
 func main() {
-	_, err := configs.LoadConfig(".")
-	if err != nil {
-		panic(err)
-	}
 
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-	db.AutoMigrate(&entity.User{}, &entity.Product{})
-
-	productDB := database.NewProduct(db)
-	productHandler := handlers.NewProductHandler(productDB)
-
-	userDB := database.NewUser(db)
-	userHandler := handlers.NewUserHandler(userDB)
-
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products", productHandler.ListProducts)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
-
-	//user
-	r.Post("/users", userHandler.Create)
-
-	http.ListenAndServe(":8000", r)
 }
